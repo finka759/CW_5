103a6d7ac1448831c8e76ace1fd9691be8b20408
finka759 <finka759@gmail.com>
2024-04-16 21:23:03 +0300

    нахождение вакансий для работодателя, один лист.

---
+++ src/db_manager.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+
+class DBManager:
+    @staticmethod
+    def create_database(database_name: str, params: dict) -> None:
+        pass
+
+    @staticmethod
+    def save_data_to_database(data: list[dict[str, Any]], db_name: str, params: dict) -> None:
+        pass
--- src/employer.py
+++ src/employer.py
@@ -1,3 +1,8 @@
+from typing import Any
+
+from src.head_hunter_api import HeadHunrterApi
+
+
 class Employer:
     @staticmethod
     def generate_emloyers_list_from_vacancies_list(vacancies_list, limit_: int = 10):
@@ -8,9 +13,9 @@ class Employer:
         top_employers = []
         count: int = 0
         while len(top_employers) < limit_:
-            # top_employers.append(Employer(vacancies_list[count].employer_id, vacancies_list[count].employer_name))
             if Employer(vacancies_list[count].employer_id, vacancies_list[count].employer_name) not in top_employers:
                 top_employers.append(Employer(vacancies_list[count].employer_id, vacancies_list[count].employer_name))
+
                 count += 1
 
         return top_employers
@@ -23,6 +28,25 @@ class Employer:
         """
         for employer in employers_list:
             print(employer)
+
+    @staticmethod
+    def get_employers_data(top_emloyers_list) -> list[dict[str, Any]]:
+        Employer.print_employers(top_emloyers_list)
+        data = []
+        vacancies_for_employer_data = []
+        next_page_token = None
+        hh_api_2 = HeadHunrterApi()
+        for employer in top_emloyers_list:
+
+            employer_data = hh_api_2.get_employer(employer.employer_id)
+            page = '0'
+            while True:
+                response = hh_api_2.get_vacancies_for_employer(employer.employer_id, page)
+                print(response)
+                vacancies_for_employer_data.extend(response['items'])
+
+
+
 
     def __init__(self, employer_id: str, name: str):
         self.employer_id = employer_id
