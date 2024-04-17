22e9400bcf713dbe45d983c81199bad457432b0b
finka759 <finka759@gmail.com>
2024-04-17 15:43:11 +0300

    вакансии добавляются в БД только с мин зарплатойю

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,11 +1,80 @@
 from typing import Any
 
+import psycopg2
+
 
 class DBManager:
     @staticmethod
     def create_database(database_name: str, params: dict) -> None:
-        pass
+        conn = psycopg2.connect(dbname='postgres', **params)
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute(f"DROP DATABASE IF EXISTS {database_name}")
+        cur.execute(f"CREATE DATABASE {database_name}")
+
+        conn.close()
+        conn = psycopg2.connect(dbname=database_name, **params)
+
+        create_employers = "CREATE TABLE employers (\
+                                           employer_id int UNIQUE PRIMARY KEY,\
+                                           name varchar(200) NOT NULL,\
+                                           url varchar(200) NOT NULL,\
+                                           vacancies_url varchar(250) NOT NULL\
+                                           );"
+
+        create_vacancy = "CREATE TABLE vacancies (\
+                                           vacancy_id int UNIQUE PRIMARY KEY,\
+                                           name varchar(200) NOT NULL,\
+                                           url varchar(200) NOT NULL,\
+                                           salary int,\
+                                           employer_id int REFERENCES employers(employer_id),\
+                                           requirements text\
+                                           );"
+
+        with conn.cursor() as cur:
+            cur.execute(create_employers)
+            cur.execute(create_vacancy)
+
+        conn.commit()
+        conn.close()
 
     @staticmethod
     def save_data_to_database(data: list[dict[str, Any]], db_name: str, params: dict) -> None:
-        pass
+
+        """Сохранение данных о работодателях и вакансиях в базу данных."""
+
+        conn = psycopg2.connect(dbname=db_name, **params)
+
+        with conn.cursor() as cur:
+            for employer in data:
+                empr_data = employer['employer']
+                cur.execute(
+                    """
+                    INSERT INTO employers (employer_id, name, url, vacancies_url)
+                    VALUES (%s, %s, %s, %s)                    
+                    """,
+                    (empr_data['id'], empr_data['name'], empr_data['site_url'],empr_data['vacancies_url'])
+                )
+
+
+                vacancyes_data = employer['vacancies']
+                for vacancy in vacancyes_data:
+                    vacy_data = vacancy
+
+                    salary_from = 0
+                    if vacy_data['salary'] is not None:
+                        if vacy_data['salary']['from'] is not None:
+                            salary_from = vacy_data.get('salary', {}).get('from', '0')
+
+                    if salary_from > 0:#добавляем в БД вакансии с указанной мин зарплатой
+                        cur.execute(
+                            """
+                            INSERT INTO vacancies (vacancy_id, name, url, salary, employer_id, requirements)
+                            VALUES (%s, %s, %s, %s, %s, %s)
+                            """,
+                            (vacy_data['id'], vacy_data['name'], vacy_data['url'], salary_from,
+                            vacy_data['employer']['id'], vacy_data['snippet']['requirement'])
+                        )
+
+        conn.commit()
+        conn.close()
--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
-from data.config import config
+from src.config import config
 from src.db_manager import DBManager
 from src.employer import Employer
 from src.head_hunter_api import HeadHunrterApi
-from src.json_saver import JsonSaver
 from src.vacancy import Vacancy
-import psycopg2
 
 
 def main():
@@ -28,13 +26,15 @@ def user_interaction():
     vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
 
     sorted_vacancies_list = Vacancy.sort_vacancies(vacancies_list)
+
     top_emloyers_list = Employer.generate_emloyers_list_from_vacancies_list(sorted_vacancies_list, 2)
     Employer.print_employers(top_emloyers_list)
     data = Employer.get_employers_data(top_emloyers_list)
     print(data)
+
     params = config()
     DBManager.create_database('top_employers', params)
-    # DBManager.save_data_to_database(data, 'top_employers', params)
+    DBManager.save_data_to_database(data, 'top_employers', params)
 
 if __name__ == "__main__":
     main()
