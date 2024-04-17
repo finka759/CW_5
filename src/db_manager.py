9028a5f86e053786993d869609b0549299fdf992
finka759 <finka759@gmail.com>
2024-04-17 19:52:29 +0300

    Последний метод не проверил

--- src/db_manager.py
+++ src/db_manager.py
@@ -4,16 +4,20 @@ import psycopg2
 
 
 class DBManager:
-    @staticmethod
-    def create_database(database_name: str, params: dict) -> None:
-        conn = psycopg2.connect(dbname='postgres', **params)
+
+    def __init__(self, database_name: str, params: dict):
+        self.database_name = database_name
+        self.params = params
+
+    def create_database(self) -> None:
+        conn = psycopg2.connect(dbname='postgres', **self.params)
         conn.autocommit = True
         cur = conn.cursor()
-        cur.execute(f"DROP DATABASE IF EXISTS {database_name}")
-        cur.execute(f"CREATE DATABASE {database_name}")
+        cur.execute(f"DROP DATABASE IF EXISTS {self.database_name}")
+        cur.execute(f"CREATE DATABASE {self.database_name}")
 
         conn.close()
-        conn = psycopg2.connect(dbname=database_name, **params)
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
 
         create_employers = "CREATE TABLE employers (\
                                            employer_id int UNIQUE PRIMARY KEY,\
@@ -38,12 +42,11 @@ class DBManager:
         conn.commit()
         conn.close()
 
-    @staticmethod
-    def save_data_to_database(data: list[dict[str, Any]], db_name: str, params: dict) -> None:
+    def save_data_to_database(self, data: list[dict[str, Any]]) -> None:
 
         """Сохранение данных о работодателях и вакансиях в базу данных."""
 
-        conn = psycopg2.connect(dbname=db_name, **params)
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
 
         with conn.cursor() as cur:
             for employer in data:
@@ -66,7 +69,7 @@ class DBManager:
                         if vacy_data['salary']['from'] is not None:
                             salary_from = vacy_data.get('salary', {}).get('from', '0')
 
-                    if salary_from > 0:#добавляем в БД вакансии с указанной мин зарплатой
+                    if salary_from > 0:#добавляем в БД вакансии только с указанной мин зарплатой
                         cur.execute(
                             """
                             INSERT INTO vacancies (vacancy_id, name, url, salary, employer_id, requirements)
@@ -78,3 +81,69 @@ class DBManager:
 
         conn.commit()
         conn.close()
+
+
+
+    def get_companies_and_vacancies_count(self):
+        '''
+        получает список всех компаний и количество вакансий у каждой компании
+        '''
+        with psycopg2.connect(dbname=self.database_name, **self.params) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT employer_id, employers.name, COUNT(*) AS vacancies_count FROM vacancies RIGHT "
+                            "JOIN employers USING(employer_id) GROUP BY employer_id;")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+
+    def get_all_vacancies(self):
+        '''
+        получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию
+        '''
+        with psycopg2.connect(dbname=self.database_name, **self.params) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "SELECT  vacancy_id, vacancies.name, requirements, vacancies.url, salary, employers.name AS "
+                    "company FROM vacancies JOIN employers USING(employer_id);")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+
+    def get_avg_salary(self):
+        '''
+        получает среднюю зарплату по вакансиям.
+        '''
+        with psycopg2.connect(dbname=self.database_name, **self.params) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "SELECT AVG(salary) as avg_salary FROM vacancies JOIN employers USING(employer_id);")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+
+    def get_vacancies_with_higher_salary(self):
+        '''
+        получает список всех вакансий, у которых зарплата выше средней по всем вакансиям
+        '''
+        with psycopg2.connect(dbname=self.database_name, **self.params) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "SELECT * FROM vacancies WHERE salary > (SELECT AVG(salary) FROM vacancies JOIN employers USING("
+                    "employer_id));")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+
+    def get_vacancies_with_keyword(self, keyword: str):
+        '''
+        получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python
+        '''
+        with psycopg2.connect(dbname=self.database_name, **self.params) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                    SELECT * FROM vacancies
+                    WHERE vacancy_name like %s
+                """, (f"%{keyword}%)",))
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
--- src/main.py
+++ src/main.py
@@ -10,31 +10,41 @@ def main():
 
 
 def user_interaction():
-    search_query = ''
-    while search_query == '':
-        search_query = input("Введите поисковый запрос: ")
-
-    salary = None
-    while salary is None:
-        salary_str = input("Введите минимальную зарплату в рублях: ")
-        if salary_str.isdigit():
-            salary = int(salary_str)
-
-    hh_api = HeadHunrterApi()
-    hh_vacancies = hh_api.get_vacancies(search_query, salary)
-
-    vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
-
-    sorted_vacancies_list = Vacancy.sort_vacancies(vacancies_list)
-
-    top_emloyers_list = Employer.generate_emloyers_list_from_vacancies_list(sorted_vacancies_list, 2)
-    Employer.print_employers(top_emloyers_list)
-    data = Employer.get_employers_data(top_emloyers_list)
-    print(data)
-
+    # search_query = ''
+    # while search_query == '':
+    #     search_query = input("Введите поисковый запрос: ")
+    #
+    # salary = None
+    # while salary is None:
+    #     salary_str = input("Введите минимальную зарплату в рублях: ")
+    #     if salary_str.isdigit():
+    #         salary = int(salary_str)
+    #
+    # hh_api = HeadHunrterApi()
+    # hh_vacancies = hh_api.get_vacancies(search_query, salary)
+    #
+    # vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
+    #
+    # sorted_vacancies_list = Vacancy.sort_vacancies(vacancies_list)
+    #
+    # top_emloyers_list = Employer.generate_emloyers_list_from_vacancies_list(sorted_vacancies_list, 2)
+    # Employer.print_employers(top_emloyers_list)
+    # data = Employer.get_employers_data(top_emloyers_list)
+    # print(data)
+    #
     params = config()
-    DBManager.create_database('top_employers', params)
-    DBManager.save_data_to_database(data, 'top_employers', params)
+    dm_manager_top_employers = DBManager('top_employers', params)
+    # dm_manager_top_employers.create_database()
+    # dm_manager_top_employers.save_data_to_database(data)
+
+    dm_manager_top_employers.get_companies_and_vacancies_count()
+    dm_manager_top_employers.get_all_vacancies()
+    dm_manager_top_employers.get_avg_salary()
+    dm_manager_top_employers.get_vacancies_with_higher_salary()
+    dm_manager_top_employers.get_vacancies_with_keyword('python')
+
+
+
 
 if __name__ == "__main__":
     main()
